342a1bb44480d1dbcc05573b2fbc868b1f57c9cf
yea-idk <62530097+yea-idk@users.noreply.github.com>
2020-05-27 12:59:02 -0400

    Update bible-modder.lua

--- bible-modder.lua
+++ bible-modder.lua
@@ -1,16 +1,16 @@
-input = io.open('./bible.txt', 'r')
+input = io.open('./bible.txt', 'r') --change this to the name of the file you wanna use
 text = input:read('*a')
 t = text:upper()
 input:close()
 rep = {
-	{ " ", "👏" },
+	{ " ", "👏" }, --{ "word 1", "word 2" }, add as many into this as you want,
 }
 print(#rep .. ' words loaded')
 for i = 1, #rep do
 	local inp = rep[i]
 	t = string.gsub( t, inp[1], inp[2] )
 end
-output = io.open('./modded-bible.txt', 'w')
+output = io.open('./modded-bible.txt', 'w') --change this to what you want the new file to be, set it to the input to overwrite that file
 output:write(t)
 output:close()
 print("done")
